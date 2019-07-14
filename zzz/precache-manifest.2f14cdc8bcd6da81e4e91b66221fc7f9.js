08db051d31fa8106c0a3d4a7229a304b82492d47
ndreyu <akudryavtsev@akudryavtsev.by>
2019-07-14 10:24:22 +0300

    zzzz

---
+++ zzz/precache-manifest.2f14cdc8bcd6da81e4e91b66221fc7f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33aa646b003258753d97",
+    "url": "/mysweetapp/static/js/runtime~main.33aa646b.js"
+  },
+  {
+    "revision": "d62388bbe5896414a73f",
+    "url": "/mysweetapp/static/js/main.d62388bb.chunk.js"
+  },
+  {
+    "revision": "62c7d93de8da00088e16",
+    "url": "/mysweetapp/static/js/2.62c7d93d.chunk.js"
+  },
+  {
+    "revision": "d62388bbe5896414a73f",
+    "url": "/mysweetapp/static/css/main.41bd1e56.chunk.css"
+  },
+  {
+    "revision": "5f610374f860a92fafd66c2aafd5b32a",
+    "url": "/mysweetapp/index.html"
+  }
+];
--- zzz/service-worker.js
+++ zzz/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8269462931cb2f55003941f2e5517598.js"
+  "/mysweetapp/precache-manifest.2f14cdc8bcd6da81e4e91b66221fc7f9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mysweetapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
