ac9bc8e1e0f3b707a1a792695dab00ef3ed148bd
ndreyu <akudryavtsev@akudryavtsev.by>
2019-07-15 13:47:20 +0300

    z1.1 undo redo 2

---
+++ zzz/precache-manifest.68075eb96ad00ac9b635d9d2ac40c53d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "4e970b01c2e331632d71",
+    "url": "/static/js/main.4e970b01.chunk.js"
+  },
+  {
+    "revision": "6821cdcafdac39dce032",
+    "url": "/static/js/2.6821cdca.chunk.js"
+  },
+  {
+    "revision": "4e970b01c2e331632d71",
+    "url": "/static/css/main.363468ec.chunk.css"
+  },
+  {
+    "revision": "b85d5bc10f43bc921f48b9d9d2edadf6",
+    "url": "/index.html"
+  }
+];
