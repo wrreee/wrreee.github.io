2cafa9ab2bcda79c0c436459c73640f275331b7e
ndreyu <akudryavtsev@akudryavtsev.by>
2019-07-15 15:26:54 +0300

    z1 ++

---
+++ zzz2/precache-manifest.ffd1ec2ecf3d2e48445f170d550808ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "61e78caf002dba4d27e3",
+    "url": "/static/js/main.61e78caf.chunk.js"
+  },
+  {
+    "revision": "6821cdcafdac39dce032",
+    "url": "/static/js/2.6821cdca.chunk.js"
+  },
+  {
+    "revision": "61e78caf002dba4d27e3",
+    "url": "/static/css/main.363468ec.chunk.css"
+  },
+  {
+    "revision": "f219c12b97571c31f1a39a038cbda936",
+    "url": "/index.html"
+  }
+];
