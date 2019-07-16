58390ad92853082e0dff9ddc87186873cdff3757
ndreyu <akudryavtsev@akudryavtsev.by>
2019-07-16 11:35:05 +0300

    z1.2 pattern size

---
+++ zzz2/precache-manifest.6106c3e1c82cd2c9d645001fab714073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6c5492e442711583272",
+    "url": "/zzz/static/js/runtime~main.b6c5492e.js"
+  },
+  {
+    "revision": "3267cd0434f193c2c66b",
+    "url": "/zzz/static/js/main.3267cd04.chunk.js"
+  },
+  {
+    "revision": "6821cdcafdac39dce032",
+    "url": "/zzz/static/js/2.6821cdca.chunk.js"
+  },
+  {
+    "revision": "3267cd0434f193c2c66b",
+    "url": "/zzz/static/css/main.363468ec.chunk.css"
+  },
+  {
+    "revision": "80c3abf180a8c54897db1845cb70db24",
+    "url": "/zzz/index.html"
+  }
+];
