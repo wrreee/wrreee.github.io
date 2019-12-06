fd675148999ae23ada85cab7dde8c20674b48c86
ndreyu <akudryavtsev@akudryavtsev.by>
2019-12-06 17:58:01 +0300

    u3u3

---
+++ u3u/precache-manifest.f63a617380d50ee73d61304c2a85257f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add08b62e43983a96f28114e6ca2e880",
+    "url": "/u3u/index.html"
+  },
+  {
+    "revision": "b173e9b4e97a1b0ec85e",
+    "url": "/u3u/static/css/main.90c4a48b.chunk.css"
+  },
+  {
+    "revision": "ac10ae5aa5c3ac07a261",
+    "url": "/u3u/static/js/2.f814a76f.chunk.js"
+  },
+  {
+    "revision": "b173e9b4e97a1b0ec85e",
+    "url": "/u3u/static/js/main.1616c0b8.chunk.js"
+  },
+  {
+    "revision": "7dc8f55e17119e4b333d",
+    "url": "/u3u/static/js/runtime-main.5cacacf2.js"
+  }
+]);
--- u3u/service-worker.js
+++ u3u/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/44sss/precache-manifest.8480920596db4acbef597932880990f9.js"
+  "/u3u/precache-manifest.f63a617380d50ee73d61304c2a85257f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/44sss/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/u3u/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
