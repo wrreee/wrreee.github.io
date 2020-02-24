ae2a1e59fbdc563dda31b0aaf5695bf67946b687
ndreyu <akudryavtsev@akudryavtsev.by>
2020-02-24 16:17:51 +0300

    bi0

---
+++ bigimage/precache-manifest.3ac1503eebdf5b7f1ee2d7d9769f2f40.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25895a922e9d8e9b653751e84f3044cc",
+    "url": "/bigimage/index.html"
+  },
+  {
+    "revision": "6fee40debf88adeb8987",
+    "url": "/bigimage/static/css/main.4c4d6c2b.chunk.css"
+  },
+  {
+    "revision": "cd62d45ed8071699d14d",
+    "url": "/bigimage/static/js/2.481f883a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bigimage/static/js/2.481f883a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fee40debf88adeb8987",
+    "url": "/bigimage/static/js/main.de514032.chunk.js"
+  },
+  {
+    "revision": "ad48df3f49cd7a2443a2",
+    "url": "/bigimage/static/js/runtime-main.a5a2a448.js"
+  },
+  {
+    "revision": "ef54ab0df4fc4680cd4059856739a125",
+    "url": "/bigimage/static/media/1.ef54ab0d.jpg"
+  },
+  {
+    "revision": "f3b19975b3af853a1b1375d70acc6979",
+    "url": "/bigimage/static/media/10.f3b19975.jpg"
+  },
+  {
+    "revision": "38f1d4205079843883f39249a15a4a31",
+    "url": "/bigimage/static/media/11.38f1d420.jpg"
+  },
+  {
+    "revision": "59e9c54be99d59a19174dbaac1e24248",
+    "url": "/bigimage/static/media/12.59e9c54b.jpg"
+  },
+  {
+    "revision": "7ce560248e9a983a2208d1bb8be230a4",
+    "url": "/bigimage/static/media/2.7ce56024.jpg"
+  },
+  {
+    "revision": "4eee792baa5d16bdeaacc135fc3c5427",
+    "url": "/bigimage/static/media/3.4eee792b.jpg"
+  },
+  {
+    "revision": "c66c947f7c7233efd00ff6d396e1d622",
+    "url": "/bigimage/static/media/4.c66c947f.jpg"
+  },
+  {
+    "revision": "0d999e115c72a0e38d3691ca10eea26f",
+    "url": "/bigimage/static/media/5.0d999e11.jpg"
+  },
+  {
+    "revision": "9e25bdb54431618de9229af2d5a329b0",
+    "url": "/bigimage/static/media/6.9e25bdb5.jpg"
+  },
+  {
+    "revision": "b520d064c5cc4ffa1e99363acce526f5",
+    "url": "/bigimage/static/media/7.b520d064.jpg"
+  },
+  {
+    "revision": "b867848ba22eedd7ae587f81fc1f4507",
+    "url": "/bigimage/static/media/8.b867848b.jpg"
+  },
+  {
+    "revision": "e0e73f79ef1e7541e8454127b21986a0",
+    "url": "/bigimage/static/media/9.e0e73f79.jpg"
+  }
+]);
