1d8471df4ddb66f37643e8db13e483a5025ca53d
ndreyu <akudryavtsev@akudryavtsev.by>
2021-08-19 18:31:47 +0300

    aup 1.1.

--- aup/main.js
+++ aup/main.js
@@ -76655,23 +76655,12 @@ var Osc = (0,react__WEBPACK_IMPORTED_MODULE_0__.forwardRef)(function (_a, ref) {
     var _b = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(false), started = _b[0], setStarted = _b[1];
     var _c = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)([]), params = _c[0], setParams = _c[1];
     (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function () {
-        function start() {
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, tone__WEBPACK_IMPORTED_MODULE_1__.start()];
-                        case 1:
-                            _a.sent();
-                            osc.current = new tone__WEBPACK_IMPORTED_MODULE_1__.Oscillator(440, "sine").toDestination();
-                            osc.current.mute = true;
-                            osc.current.start();
-                            return [2 /*return*/];
-                    }
-                });
-            });
-        }
-        start();
+        osc.current = new tone__WEBPACK_IMPORTED_MODULE_1__.Oscillator(440, "sine").toDestination();
+        osc.current.mute = true;
+        osc.current.start();
         return function () {
+            var _a;
+            (_a = osc.current) === null || _a === void 0 ? void 0 : _a.dispose();
         };
     }, [osc]);
     (0,react__WEBPACK_IMPORTED_MODULE_0__.useImperativeHandle)(ref, function () { return ({
@@ -76851,8 +76840,45 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "Synth1": () => (/* binding */ Synth1)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var _Osc__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Osc */ "./app/root/Synth1/Osc.tsx");
-/* harmony import */ var _Synth1_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Synth1.css */ "./app/root/Synth1/Synth1.css");
+/* harmony import */ var tone__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! tone */ "./node_modules/tone/build/esm/index.js");
+/* harmony import */ var _Osc__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Osc */ "./app/root/Synth1/Osc.tsx");
+/* harmony import */ var _Synth1_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./Synth1.css */ "./app/root/Synth1/Synth1.css");
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __spreadArray = (undefined && undefined.__spreadArray) || function (to, from) {
     for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
         to[j] = from[i];
@@ -76862,13 +76888,22 @@ var __spreadArray = (undefined && undefined.__spreadArray) || function (to, from
 
 
 
+
 var Synth1 = function (props) {
     var _a = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)([]), oscs = _a[0], setOscs = _a[1];
     var oscsRefs = (0,react__WEBPACK_IMPORTED_MODULE_0__.useRef)([]);
-    var handleAddOsc = (0,react__WEBPACK_IMPORTED_MODULE_0__.useCallback)(function () {
-        oscsRefs.current.push((0,react__WEBPACK_IMPORTED_MODULE_0__.createRef)());
-        setOscs(__spreadArray(__spreadArray([], oscs), [{}]));
-    }, [oscs]);
+    var handleAddOsc = (0,react__WEBPACK_IMPORTED_MODULE_0__.useCallback)(function () { return __awaiter(void 0, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, tone__WEBPACK_IMPORTED_MODULE_1__.start()];
+                case 1:
+                    _a.sent();
+                    oscsRefs.current.push((0,react__WEBPACK_IMPORTED_MODULE_0__.createRef)());
+                    setOscs(__spreadArray(__spreadArray([], oscs), [{}]));
+                    return [2 /*return*/];
+            }
+        });
+    }); }, [oscs]);
     var handleConnect = react__WEBPACK_IMPORTED_MODULE_0__.useCallback(function (number, signal) {
         var _a, _b;
         console.log(oscsRefs.current);
@@ -76877,7 +76912,7 @@ var Synth1 = function (props) {
     return (react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", { className: 'synth1Container' },
         react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", { className: 'oscillators' },
             oscs.map(function (_, index) {
-                return react__WEBPACK_IMPORTED_MODULE_0__.createElement(_Osc__WEBPACK_IMPORTED_MODULE_1__.Osc, { ref: oscsRefs.current[index], key: index, index: index, onConnectRequest: handleConnect });
+                return react__WEBPACK_IMPORTED_MODULE_0__.createElement(_Osc__WEBPACK_IMPORTED_MODULE_2__.Osc, { ref: oscsRefs.current[index], key: index, index: index, onConnectRequest: handleConnect });
             }),
             react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", { className: "addOscButtonContainer" },
                 react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", { title: 'add oscillator', className: "addOscButton", onClick: handleAddOsc }, "+")))));
